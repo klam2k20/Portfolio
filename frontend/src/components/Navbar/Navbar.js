6000605da8cff0ce5ba4ef10bca37a2aa04ccf1e
klam2k20 <klam2k17@gmail.com>
2023-02-17 00:48:57 -0500

    navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiOutlineMenu, HiX } from "react-icons/hi";
+import { easeInOut, easeOut, motion } from "framer-motion";
 import "./Navbar.scss";
 
+import images from "../../constants/images";
+
 function Navbar() {
-  return <h1>Navbar</h1>;
+  const [toggle, setToggle] = useState(false);
+  const links = ["home", "about", "work", "skills", "contact"];
+
+  return (
+    <nav className='app_navbar'>
+      <div className='app_navbar-logo'>
+        <img src={images.logo} alt='logo' />
+      </div>
+      <ul className='app_navbar-links'>
+        {links.map((link) => (
+          <li className='app_flex p_text' key={`link-${link}`}>
+            <div />
+            <a href={`#${link}`}>{link}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className='app_navbar-menu'>
+        {!toggle && <HiOutlineMenu onClick={() => setToggle(true)} />}
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [250, 0] }}
+            transition={{ duration: 0.75, ease: "easeOut" }}>
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {links.map((link) => (
+                <li className='app_flex p_text' key={`link-${link}`}>
+                  <a href={`#${link}`}>{link}</a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
---
+++ frontend/src/constants/images.js
@@ -0,0 +1,9 @@
+import bgWhite from "../assets/bgWhite.png";
+import logo from "../assets/logo.png";
+
+const images = {
+  bgWhite,
+  logo,
+};
+
+export default images;

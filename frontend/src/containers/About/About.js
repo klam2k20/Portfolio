8a20fedc66805e37f43cad05bc9c23010b20df8b
klam2k20 <klam2k17@gmail.com>
2023-02-21 16:57:06 -0500

    About me section

--- frontend/src/containers/About/About.js
+++ frontend/src/containers/About/About.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Wrapper from "../Wrapper/Wrapper";
+import { motion } from "framer-motion";
+import { client, imageFor } from "../../client";
 import "./About.scss";
 
 function About() {
-  return <h1 id='about'>About</h1>;
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "about"]';
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
+  return (
+    <div id='about' className='app_about'>
+      <h1 className='head_text'>
+        About <span>Me</span>
+      </h1>
+      <div className='app_about-items'>
+        {abouts.map((about) => (
+          <motion.div key={`about ${about.title}`} className='app_about-item'>
+            <div className='app_about-item-title'>
+              <img src={imageFor(about.imgUrl)} alt='about item' />
+              <h2 className='bold_text'>{about.title}</h2>
+            </div>
+            <div>
+              <p className='app_about-item-description'>{about.description}</p>
+            </div>
+          </motion.div>
+        ))}
+        <div className='app_about-quote'>
+          <div />
+          <span>
+            <span className='app_about-quote-text'>Only I Can Call My Dream Stupid!</span>
+            <br />
+            <span className='p_text'>- Zoro, 'One Piece'</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Wrapper(About, "about");

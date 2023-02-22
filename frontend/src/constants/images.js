fc809b0c2e10b702b71547cff3a0b911a23b5c43
klam2k20 <klam2k17@gmail.com>
2023-02-22 15:18:58 -0500

    removed unused images

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,19 +1,11 @@
 import bgWhite from "../assets/bgWhite.png";
 import logo from "../assets/logo.png";
 import profile from "../assets/profile.png";
-import react from "../assets/react.png";
-import mongodb from "../assets/mongodb.png";
-import css from "../assets/css.png";
-import git from "../assets/git.png";
 
 const images = {
   bgWhite,
   logo,
   profile,
-  react,
-  mongodb,
-  css,
-  git,
 };
 
 export default images;

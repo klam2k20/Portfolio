74cf7e70736d4327a03a6a5dc8c87aecc01c6941
klam2k20 <klam2k17@gmail.com>
2023-02-21 20:36:17 -0500

    redo hero section

--- frontend/src/containers/Hero/Hero.js
+++ frontend/src/containers/Hero/Hero.js
@@ -4,32 +4,57 @@ import { motion } from "framer-motion";
 import images from "../../constants/images";
 import "./Hero.scss";
 
+const sentence = {
+  hidden: { opacity: 1 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delayChildren: 2,
+      staggerChildren: 0.25,
+    },
+  },
+};
+const letter = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
+
 function Hero() {
+  const name = "Kenny";
+
   return (
     <div id='home'>
       <motion.div
         className='app_hero-info'
-        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ type: "spring", duration: 1, ease: "easeOut", bounce: 0.5 }}>
+        animate={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delay: 0.5 }}>
         <div className='app_hero-intro'>
-          <span>👋🏻</span>
-          <div>
-            <p className='p_text'>Hello, I'm</p>
-            <h1 className='head_text'>Kenny</h1>
-          </div>
+          <motion.span
+            animate={{ scale: [0, 1] }}
+            transition={{ type: "spring", bounce: 0.5, duration: 1, delay: 1 }}>
+            Hello, It's Me
+          </motion.span>
+          <motion.div variants={sentence} initial='hidden' animate='visible'>
+            {name.split("").map((char, i) => (
+              <motion.h1 key={`${char}-${i}`} variants={letter}>
+                {char}
+              </motion.h1>
+            ))}
+          </motion.div>
         </div>
-        <div className='app_hero-position'>
+
+        <motion.div
+          className='app_hero-position'
+          animate={{ y: [100, 0], opacity: [0, 1] }}
+          transition={{ duration: 0.5, delay: 3.5 }}>
           <p className='p_text'>Aspiring Full-Stack Developer</p>
           <p className='p_text'>CS/CE NEU @2022</p>
           <p className='p_text'>Based in NYC</p>
-        </div>
+        </motion.div>
       </motion.div>
-      <motion.div
-        className='app_hero-profile'
-        whileInView={{ opacity: [0, 1] }}
-        transition={{ duration: 1, delayChildren: 0.5 }}>
+      <div className='app_hero-profile'>
         <img src={images.profile} alt='profile' />
-      </motion.div>
+      </div>
     </div>
   );
 }

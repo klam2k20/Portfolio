776a5183d294c682499027e9130e8e9fb23de6e2
klam2k20 <klam2k17@gmail.com>
2023-02-22 15:09:04 -0500

    Move sanity details to env

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,12 +2,11 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: "ub83se4o",
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
   useCdn: false,
   apiVersion: "2022-02-01",
-  token:
-    "skEbj54H35aFLPBBZ0QKVBsiO9wHRWQke8HMaCjqclSFL9NlXPGsQD6xUECUd00IBak6CnA2BmmOYC5dHG0GiC3cznAk84tfSmZiwcis0P1Cy88GhwukuW2oG9BmIUohVAZfqgOCxSYa8oxBSjHQsiQyHWG6N1eWYtOdbfyo9R74Pzvleppo",
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
